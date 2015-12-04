21fd3a0f4215205d6ebe84adeb6c9d88796850f8
Charles Leifer <coleifer@gmail.com>
2015-12-04 11:33:05 -0600

    Add example

---
+++ examples/regex_search.py
@@ -0,0 +1,29 @@
+import re
+import sqlite3
+
+from vtfunc import TableFunction
+
+
+class RegexSearch(TableFunction):
+    params = ['regex', 'search_string']
+    columns = ['match']
+    name = 'regex_search'
+
+    def initialize(self, regex=None, search_string=None):
+        self._iter = re.finditer(regex, search_string)
+
+    def iterate(self, idx):
+        # We do not need `idx`, so just ignore it.
+        return (next(self._iter).group(0),)
+
+
+conn = sqlite3.connect(':memory:')
+
+# Register the module with the connection.
+rgx_module = RegexSearch()
+rgx_module.register(conn)
+
+# Query the module.
+query_params = ('[0-9]+', '1337 foo 567 bar 999 baz 0123')
+cursor = conn.execute('SELECT * FROM regex_search(?, ?);', query_params)
+print cursor.fetchall()
