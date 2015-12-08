dda77955f5ee6620f62735571ae6f6499d0987da
Charles Leifer <coleifer@gmail.com>
2015-12-08 11:05:42 -0600

    Version 0.1.6

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ extension = Extension(
 
 setup(
     name='vtfunc',
-    version='0.1.5',
+    version='0.1.6',
     description='',
     url='https://github.com/coleifer/sqlite-vtfunc',
     author='Charles Leifer',
