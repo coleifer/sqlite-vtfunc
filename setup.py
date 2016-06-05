4304eb19ea40d69ac2663e2438c23069682189bf
Charles Leifer <coleifer@gmail.com>
2016-06-04 23:36:49 -0500

    Add cython as dependency.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     version='0.2.1',
     description='Tabular user-defined functions for SQLite3.',
     url='https://github.com/coleifer/sqlite-vtfunc',
-    install_requires=['pysqlite>=2.8.2'],
+    install_requires=['pysqlite>=2.8.2', 'Cython'],
     dependency_links=[
         'https://github.com/coleifer/pysqlite/zipball/master#egg=pysqlite',
     ],
